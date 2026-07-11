f0e6aa691e0f68e19b9c53ab0f3b65c83df5eb11
MBurkeski <morgan.burke@bellevuecollege.edu>
2026-07-11 00:18:12 +0000

    Lab 1 Assigment Completed

--- labwork/lab01-input-output/main.py
+++ labwork/lab01-input-output/main.py
@@ -1,10 +1,42 @@
 # DEV 108 Lab Activity 1
-# [Today's Date]
-# [Your Name]
+# July 10 2026
+# Morgan Burke
 
 # Start writing your code below.
 # Click the "Run" button (or run this file in the terminal with: python main.py)
 # to see your output in the terminal window.
 # Good luck! You can do this - please don't use any AI tools to do this assignment. 
 # That violates the AI Policy for this class and puts your learning at a disadvantage. 
+
+print("Hello world")
+
+num1 = 10
+num2 = 55
+print(f"{num1} + {num2} = {num1 + num2}")
+
+num3 = 5
+num4 = 2
+print(f"{num3} x {num1} x {num4} = {num3 * num1 * num4}")
+
+firstName = "Morgan"
+lastName = "Burke"
+majors = "molecular bioscience and nursing"
+print("My name is", firstName, lastName, ".")
+print("My majors are", majors, ".")
+
+num5 = input("Pick a number from 0 to 100.")
+print("You entered", num5, ".")
+
+num6 = input("Pick a number from 10 to 10000.")
+print("You entered", num6, ".")
+
+num2 = 55
+num7 = 99
+print(f"{num2} x {num7} = {num2 * num7}")
+
+name = input("What is your name?")
+print("Nice to meet you,", name, ".")
+
+age = input("How old are you?")
+print(name,"is", age, "years old.")
 
